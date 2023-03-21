63b46f48fdab66cc6f8a3bdf4fcf5d21b964b7d9
Pavel Bazhin <p81181@yandex.ru>
2023-03-21 23:52:18 +0300

    Добавил условие

---
+++ EXAMPLE005_If/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите Ваше имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "павел")
+{
+    Console.Write ("Ура это ПАВЕЛ!! ");
+}
+else
+{Console.Write ("Привет, ");
+Console.WriteLine (username);
+}
