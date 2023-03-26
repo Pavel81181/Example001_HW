81f575669ef5c995c2510ae4adf1fb2880c01056
Pavel Bazhin <p81181@yandex.ru>
2023-03-26 10:30:32 +0300

    Добавил описание функции

---
+++ EXAMPLE8_FUNCTIONS/Program.cs
@@ -0,0 +1,31 @@
+﻿//double f(double x)
+//{double result = x * x + 1;
+//reurn result;
+//}
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 12;
+int b1 = 23;
+int c1 = 484;
+int a2 = 13;
+int b2 = 8;
+int c2 = 2206;
+int a3 = 89990;
+int b3 = 25;
+int c3 = 3;
+
+int max1 = Max(a1,b1,c1);
+int max2 = Max(a2,b2,c2);
+int max3 = Max(a3,b3,c3);
+int max = Max(max1,max2,max3);
+
+Console.WriteLine(max);
+
+int maxim = Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
+
+Console.WriteLine(maxim);
